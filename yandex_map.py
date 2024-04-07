0ac8848186485cbd3ddd5ede8ef3297586ba1e67
kate9011 <143648137+kate9011@users.noreply.github.com>
2024-04-07 17:19:15 +0300

    Add files via upload

---
+++ yandex_map.py
@@ -0,0 +1,67 @@
+import requests
+from pprint import pprint
+# url = r"https://static-maps.yandex.ru/v1?ll=37.620070,55.753630&size=450,450&z=13&pt=37.620070,55.753630,pmwtm1~37.64,55.76363,pmwtm99&apikey=9d7592b8-29c0-4693-ba7b-ef2b24ff62b1"
+# r = requests.get(url)
+
+# url= r"https://static-maps.yandex.ru/v1?lang=ru_RU&ll=37.63199,55.81039&spn=0.016457,0.00619&apikey=9d7592b8-29c0-4693-ba7b-ef2b24ff62b1"
+address = input()
+
+if address=="" or address.count(" ")==len(address):
+    print('Введите адрес!')
+else:
+    url = f"https://geocode-maps.yandex.ru/1.x/?apikey=b2f42f9a-efc2-4a51-b051-5dbd204fa768&geocode={address}&format=json"
+    r = requests.get(url)
+    print(r.status_code)
+    geo_code = r.json()
+    coordinates = geo_code['response']
+    coordinates_1 = coordinates['GeoObjectCollection']
+    coordinates_2 = coordinates_1['featureMember']
+    coordinates_3 = coordinates_2[0]
+    coordinates_4 = coordinates_3['GeoObject']
+    coordinates_5 = coordinates_4['Point']
+    coordinates_6 = coordinates_5['pos']
+    coordinates_6_1 = coordinates_6.split()
+    first_coordinate = coordinates_6_1[0]
+    second_coordinate = coordinates_6_1[1]
+
+    url_2 = f"https://static-maps.yandex.ru/v1?lang=ru_RU&ll={first_coordinate},{second_coordinate}&spn=0.016457,0.00619&apikey=9d7592b8-29c0-4693-ba7b-ef2b24ff62b1"
+    image = requests.get(url_2)
+    pprint(image)
+    line = image.content
+    file = open('image.png', 'wb')
+    file.write(line)
+
+# url = f"https://geocode-maps.yandex.ru/1.x/?apikey=b2f42f9a-efc2-4a51-b051-5dbd204fa768&geocode={adress}&format=json"
+# r = requests.get(url)
+# print(r.status_code)
+
+# print(r.text)
+# pprint(r.json())
+# geo_code = r.json()
+# coordinates = geo_code['response']
+# coordinates_1 = coordinates['GeoObjectCollection']
+# coordinates_2 = coordinates_1['featureMember']
+# coordinates_3 = coordinates_2[0]
+# coordinates_4  =coordinates_3['GeoObject']
+# coordinates_5  =coordinates_4['Point']
+# coordinates_6  =coordinates_5['pos']
+# coordinates_6_1 = coordinates_6.split()
+# first_coordinate = coordinates_6_1[0]
+# second_coordinate = coordinates_6_1[1]
+#
+#
+#
+# url_2= f"https://static-maps.yandex.ru/v1?lang=ru_RU&ll={first_coordinate},{second_coordinate}&spn=0.016457,0.00619&apikey=9d7592b8-29c0-4693-ba7b-ef2b24ff62b1"
+# image = requests.get(url_2)
+# pprint(image)
+# line = image.content
+# file = open('image.png', 'wb')
+# file.write(line)
+#
+
+
+
+#если несуществуюащя улица - координат нет
+#учитывать, что город только москва
+
+
